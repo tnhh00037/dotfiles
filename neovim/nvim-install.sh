6aa8ac0eee038a62d42158e15feface997499007
Nghia Phan <nghiaphan1407@gmail.com>
2020-06-07 16:59:40 +0700

    feat: vim install

---
+++ neovim/nvim-install.sh
@@ -0,0 +1,46 @@
+function cmd_exists () {
+    command -v $1 > /dev/null 2>&1
+}
+
+## Install vim
+# Should install with apt instead of pip
+
+# Linux
+if [[ $OSTYPE == "linux-gnu"* ]]
+then
+ cmd_exists nvim ; [[ "$?" -eq 1 ]] \
+&&  sudo apt install neovim 
+fi
+
+# OSX
+if [[ $OSTYPE == "darwin"* ]]
+then
+ cmd_exists nvim ; [[ "$?" -eq 1 ]] \
+&&  brew install neovim
+fi
+
+ # Make folder for vim config``
+[[ ! -d ~/.config ]] && mkdir -p ~/.config/nvim
+
+# Install virtualenv to containerize dependencies
+python3 -m pip install virtualenv
+python3 -m virtualenv -p python3 ~/.config/nvim/env
+source ~/.config/nvim/env/bin/activate
+
+# Install python dependencies
+pip install neovim jedi psutil setproctitle yapf pynvim
+deactivate
+
+# Intall neovim for node
+npm install -g neovim
+
+# Load vim-plug-manager
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Enter Neovim and install plugins using a temporary init.vim, which avoids warnings about missing colorschemes, functions, etc
+sed '/call plug#end/q' init.vim > ~/.config/nvim/init.vim # Default vim-config
+nvim -c ':PlugInstall' -c ':UpdateRemotePlugins' -c ':qall'
+rm ~/.config/nvim/init.vim
+
+# Copy personal-config to override default
+cp init.vim ~/.config/nvim/init.vim
